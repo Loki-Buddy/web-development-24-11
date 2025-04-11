6fe7a025a23be520e5c869cfda6962d0e74ef162
Loki-Buddy <jm.conring@live.de>
2025-04-11 13:29:38 +0200

    Branch übernahme

--- TierheimApiMitSQlite/index.js
+++ TierheimApiMitSQlite/index.js
@@ -1,53 +1,58 @@
-const express = require("express")
-const app = express()
-const sqlite3 = require("sqlite3")
+const express = require("express");
+const app = express();
+const sqlite3 = require("sqlite3");
 
-const db = new sqlite3.Database(":memory:")
+const db = new sqlite3.Database(":memory:");
 
 db.serialize(() => {
-   db.run( `CREATE TABLE tiere (
-    id INTEGER PRIMARY KEY,
-    tierart VARCHAR(50),
-    name VARCHAR(50),
-    krankheit VARCHAR(100),
-    age INT,
-    gewicht REAL);`)
-    db.run(`INSERT INTO tiere(tierart,name,krankheit,age,gewicht) VALUES ("Hund","Bello","husten",5,12.4)`)
+    db.run(`CREATE TABLE tiere (
+            id INTEGER PRIMARY KEY,
+            tierart VARCHAR(50),
+            name VARCHAR(50),
+            krankheit VARCHAR(100),
+            age INT,
+            gewicht REAL);`);
 
-    selectAllTiereQuery = `SELECT * FROM tiere`
-    db.all(selectAllTiereQuery, (err,rows) => {
-        if(err){
-            console.log(err)
-        }else {
-            console.log(rows)
+    db.run(`INSERT INTO tiere(tierart,name,krankheit,age,gewicht)
+            VALUES ("Hund","Bello","husten",5,12.4);`);
+
+    const selectAllTiereQuery = `SELECT * FROM tiere;`;
+    db.all(selectAllTiereQuery, (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(rows);
         }
-    })
+    });
+    
     process.on("exit", () => {
-        db.close()
-    })
-})
+        db.close();
+    });
+});
 
-app.use(express.json()) // Ermöglicht Express Json aus einem Body auszulesen
-app.get("/", (req,res) => {
-    res.send("Die API funktioniert!")
-})
+app.use(express.json()); // Ermöglicht Express Json aus einem Body auszulesen
+app.get("/", (req, res) => {
+    res.send("Die API funktioniert!");
+});
 
-app.get("/tiere", (req,res) => {
-    db.all(selectAllTiereQuery, (err,rows) => {
-        if(err){
-            res.status(404).send("Fehler in deiner Query Anfrage")
-        }else {
-            res.json(rows)
+app.get("/tiere", (req, res) => {
+    const selectAllTiereQuery = `SELECT * FROM tiere;`;
+    db.all(selectAllTiereQuery, (err, rows) => {
+        if (err) {
+            res.status(404).send("Fehler in deiner Query Anfrage");
+        } else {
+            res.json(rows);
         }
-    })
-})
+    });
+});
 
-app.post("/tiere", (req,res) => {
-    const {tierart, name, krankheit, age, gewicht} = req.body
-    db.run(`INSERT INTO tiere (tierart,name,krankheit,age,gewicht) VALUES(?,?,?,?,?)`,[tierart,name,krankheit,age,gewicht])
-    res.status(201).send("Tier wurde erfolgreich hinzugefügt")
-})
-app.listen(3000)
+app.post("/tiere", (req, res) => {
+    const { tierart, name, krankheit, age, gewicht } = req.body;
+    db.run(`INSERT INTO tiere (tierart,name,krankheit,age,gewicht) VALUES(?,?,?,?,?);`, [tierart, name, krankheit, age, gewicht]);
+    res.status(201).send("Tier wurde erfolgreich hinzugefügt");
+});
+
+app.listen(3000);
 
 
 
